6e7d1fc6fd4ae4a1e4a35429777d3444e4a1f1f5
feroz-istar <feroz@istarindia.com>
2019-05-31 12:50:58 +0530

    hbhbhb

--- src/main/java/rest/login/UploadFileService.java
+++ src/main/java/rest/login/UploadFileService.java
@@ -32,7 +32,10 @@ public class UploadFileService {
 
 		String output = "File uploaded to : " + uploadedFileLocation;
 
-		return Response.status(200).entity(output).build();
+		return Response.status(200).entity(output)
+				.header("Access-Control-Allow-Origin", "*")
+				.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+				.allow("OPTIONS").build();
 
 	}
 
