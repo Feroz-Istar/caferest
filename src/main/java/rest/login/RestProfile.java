59aad89ad466cd5a326e531ca47dbfc455225cf2
feroz-istar <feroz@istarindia.com>
2019-06-14 18:42:39 +0530

    changes in profile

--- src/main/java/rest/login/RestProfile.java
+++ src/main/java/rest/login/RestProfile.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -63,14 +64,14 @@ public class RestProfile {
 
 		// find the user whether its exist in the database or not if exits no need to
 		// insert the user
-		
-		String sql = "INSERT INTO public.cafeuser ( email, password, created_at, updated_at) VALUES( '" + email
-				+ "', '" + password + "', now(), NULL) ON conflict (email)  DO update set password ='"+password+"' returning id";
+
+		String sql = "INSERT INTO public.cafeuser ( email, password, created_at, updated_at) VALUES( '" + email + "', '" + password
+							+ "', now(), NULL) ON conflict (email)  DO update set password ='" + password + "' returning id";
 		// CafeeDBUtils.getInstance().insertIntoDB("");
 		int id = CafeeDBUtils.getInstance().insertIntoDBWithGeneratedKey(sql);
 
-		String profile_sql = "insert into user_profile(name, dob,user_id) values ('" + name + "','"
-				+ new java.sql.Timestamp(dob1.getTime()) + "',"+id+") on conflict(user_id) do nothing";
+		String profile_sql = "insert into user_profile(name, dob,user_id) values ('" + name + "','" + new java.sql.Timestamp(dob1.getTime()) + "'," + id
+							+ ") on conflict(user_id) do nothing";
 		CafeeDBUtils.getInstance().insertIntoDB(profile_sql);
 
 		for (Integer integer : roles) {
@@ -78,11 +79,70 @@ public class RestProfile {
 			CafeeDBUtils.getInstance().insertIntoDB(role_sql);
 
 		}
-		String query = "select * from cafeuser where id="+id;
-		ArrayList<HashMap<String, String>> results = CafeeDBUtils.getInstance()
-				.executeQuery(Thread.currentThread().getStackTrace(), query);
-		
+		String query = "select * from cafeuser where id=" + id;
+		ArrayList<HashMap<String, String>> results = CafeeDBUtils.getInstance().executeQuery(Thread.currentThread().getStackTrace(), query);
+
 		return Response.status(200).entity(gson.toJson(results)).header("Access-Control-Allow-Origin", "*")
-				.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT").allow("OPTIONS").build();
+							.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT").allow("OPTIONS").build();
+	}
+
+	@POST
+	@Path("/upload")
+	public Response loadProfile(String body) throws ParseException {
+		JsonParser parser = new JsonParser();
+		JsonObject json = parser.parse(body).getAsJsonObject();
+		String email = json.get("email").getAsString();
+		String password = json.get("password").getAsString();
+		String name = json.get("name").getAsString();
+		String sql = "INSERT INTO public.cafeuser ( email, password, created_at, updated_at) VALUES( '" + email + "', '" + password
+							+ "', now(), NULL) ON conflict (email)  DO update set password ='" + password + "' returning id";
+		int user_id = CafeeDBUtils.getInstance().insertIntoDBWithGeneratedKey(sql);
+		String dob = json.get("dob").getAsString();
+		Date dob1 = new SimpleDateFormat("yyyy-MM-dd").parse(dob);
+		String profile_url = json.get("profile_url").getAsString();;
+		int age = calculateAge(dob1);
+		String profile_sql = "insert into user_profile(name, dob,user_id,age,profile_image) values ('" + name + "','" + new java.sql.Timestamp(dob1.getTime()) + "',"
+							+ user_id + "," + age + ",'"+profile_url+"') on conflict(user_id) do nothing";
+		CafeeDBUtils.getInstance().insertIntoDB(profile_sql);
+		JsonArray selectedItems = json.get("selectedItems").getAsJsonArray();
+		String role_id = "";
+		/*
+		 * for (JsonElement jsonElement : selectedItems) {
+		 * role_id+=jsonElement.getAsJsonObject().get("id")+","; }
+		 */
+		List<Integer> roles = new ArrayList<>();
+		for (JsonElement selectedItem : selectedItems) {
+			roles.add(selectedItem.getAsJsonObject().get("id").getAsInt());
+		}
+		
+		for (Integer integer : roles) {
+			String role_sql = "insert into user_role (userid, role_id) values (" + user_id + "," + integer + ") on conflict do nothing";
+			CafeeDBUtils.getInstance().insertIntoDB(role_sql);
+
+		}
+		String query = "select * from cafeuser where id=" + user_id;
+		ArrayList<HashMap<String, String>> results = CafeeDBUtils.getInstance().executeQuery(Thread.currentThread().getStackTrace(), query);
+
+		return Response.status(200).entity(gson.toJson(results)).header("Access-Control-Allow-Origin", "*")
+							.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT").allow("OPTIONS").build();
+	}
+
+	public static int calculateAge(Date birthdate) {
+		Calendar birth = Calendar.getInstance();
+		birth.setTime(birthdate);
+		Calendar today = Calendar.getInstance();
+
+		int yearDifference = today.get(Calendar.YEAR) - birth.get(Calendar.YEAR);
+
+		if (today.get(Calendar.MONTH) < birth.get(Calendar.MONTH)) {
+			yearDifference--;
+		} else {
+			if (today.get(Calendar.MONTH) == birth.get(Calendar.MONTH) && today.get(Calendar.DAY_OF_MONTH) < birth.get(Calendar.DAY_OF_MONTH)) {
+				yearDifference--;
+			}
+
+		}
+
+		return yearDifference;
 	}
 }
--- src/main/java/rest/login/UploadFileService.java
+++ src/main/java/rest/login/UploadFileService.java
@@ -5,44 +5,59 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.text.NumberFormat;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.OPTIONS;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.glassfish.jersey.media.multipart.FormDataParam;
+
+import com.google.api.client.json.Json;
+import com.google.gson.JsonObject;
+
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 
 @Path("/file")
 public class UploadFileService {
 
+	private static final String FILE_FOLDER = "/tmp/feroz/";
+
 	@POST
 	@Path("/upload")
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
-	public Response uploadFile(
-			@FormDataParam("file") InputStream uploadedInputStream,
-		@FormDataParam("file") FormDataContentDisposition fileDetail) {
-		
-		String uploadedFileLocation = "/tmp/feroz/" + fileDetail.getFileName();
+	public Response uploadFile(@FormDataParam("file") InputStream uploadedInputStream, @FormDataParam("file") FormDataContentDisposition fileDetail) {
+
+		String uploadedFileLocation = FILE_FOLDER + fileDetail.getFileName();
 
 		// save it
 		writeToFile(uploadedInputStream, uploadedFileLocation);
 
 		String output = "File uploaded to : " + uploadedFileLocation;
+		System.out.println(output);
+		JsonObject jsonObject = new JsonObject();
 
-		return Response.status(200).entity(output).build();
+		jsonObject.addProperty("success", true);
+		jsonObject.addProperty("status", "success");
+		jsonObject.addProperty("filename", fileDetail.getFileName());
+		return Response.status(200).entity(jsonObject.toString()).header("Access-Control-Allow-Origin", "*")
+							.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT").allow("OPTIONS").build();
 
 	}
 
 	// save uploaded file to new location
-	private void writeToFile(InputStream uploadedInputStream,
-		String uploadedFileLocation) {
+	private void writeToFile(InputStream uploadedInputStream, String uploadedFileLocation) {
 
 		try {
-			OutputStream out = new FileOutputStream(new File(
-					uploadedFileLocation));
+			OutputStream out = new FileOutputStream(new File(uploadedFileLocation));
 			int read = 0;
 			byte[] bytes = new byte[1024];
 
@@ -59,4 +74,50 @@ public class UploadFileService {
 
 	}
 
+	@OPTIONS
+	@Path("/upload")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response uploadFile() {
+		return Response.status(200).entity("success").header("Access-Control-Allow-Origin", "*")
+							.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS")
+							.header("Access-Control-Allow-Headers", "accept, Cache-Control, content-type, x-requested-with").allow("OPTIONS")
+							.build();
+	}
+
+	@GET
+	@Path("/download")
+	@Produces(MediaType.APPLICATION_OCTET_STREAM)
+	public Response downloadFilebyQuery(@QueryParam("filename") String fileName) {
+		return download(fileName);
+	}
+
+	@GET
+	@Path("/download/{filename}")
+	@Produces(MediaType.APPLICATION_OCTET_STREAM)
+	public Response downloadFilebyPath(@PathParam("filename") String fileName) {
+		return download(fileName);
+	}
+
+	private Response download(String fileName) {
+		String fileLocation = FILE_FOLDER + fileName;
+		Response response = null;
+		NumberFormat myFormat = NumberFormat.getInstance();
+		myFormat.setGroupingUsed(true);
+
+		// Retrieve the file
+		File file = new File(FILE_FOLDER + fileName);
+		if (file.exists()) {
+			ResponseBuilder builder = Response.ok(file);
+			builder.header("Content-Disposition", "attachment; filename=" + file.getName());
+			response = builder.build();
+
+			long file_size = file.length();
+
+		} else {
+
+			response = Response.status(404).entity("FILE NOT FOUND: " + fileLocation).type("text/plain").build();
+		}
+
+		return response;
+	}
 }
